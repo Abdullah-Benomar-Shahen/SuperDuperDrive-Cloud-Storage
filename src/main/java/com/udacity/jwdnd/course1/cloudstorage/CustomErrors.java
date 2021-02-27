0e9d865809c0f98cf2ae34cf820ba5935834b5cb
Benomar Abdullah <dev.shahen.abdullah@gmail.com>
2021-02-27 22:00:05 +0100

    Added error handling for FIle Size Limit

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/CustomErrors.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/CustomErrors.java
@@ -5,6 +5,7 @@ public class CustomErrors {
     public final static String UI_ERROR_GENERAL = "Something went wrong..";
 
     public final static String UI_ERROR_FILE_IS_EMPTY = "File is empty! Please choose another file.";
+    public final static String UI_ERROR_FILE_IS_LARGE = "File size has exceeded the max. allowed size of 5MB.";
     public final static String UI_ERROR_FILE_ALREADY_EXISTS = "A file with same name already exists!";
     public final static String UI_ERROR_FILE_DELETION_FAILED = "Error while deleting file";
     public final static String UI_ERROR_LONG_NOTE_DESCRIPTION = "Note Description exceeded the max. length of 1000 character";
